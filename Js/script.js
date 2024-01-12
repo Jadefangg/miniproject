f8ee8672741f97c2728e7e427f819d6aeaee76fb
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-01-12 16:32:01 +0530

    1.9
    1.9 solving for live link

--- Js/script.js
+++ Js/script.js
@@ -1,23 +1,25 @@
 function newItem(){
   
-    let li = $('<li></li>');
-    let inputValue = $('#input').val(); 
-    li.append(inputValue); 
-    
-    if ($('#input').val() === '') {
-      alert("You must write something!");
-    } else {
-      let list = $('#list');
-       list.append(li);
-    } 
-    li.on('dblclick', function (){
-        li.toggleClass('strike');
-    });
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-  $(crossOutButton).append('X');
-  li.append(crossOutButton);
-
-crossOutButton.on('click', function (){
-    li.addClass('delete');
+  let li = $('<li></li>');
+  let inputValue = $('#input').val(); 
+  li.append(inputValue); 
+  
+  if ($('#input').val() === '') {
+    alert("You must write something!");
+  } else {
+    let list = $('#list');
+     list.append(li);
+  } 
+  li.on('dblclick', function (){
+      li.toggleClass('strike');
   });
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+    $(crossOutButton).append('X');
+    li.append(crossOutButton);
+  crossOutButton.on('click', function (){
+      li.addClass('delete');
+    });
+  $('#list').sortable();
 }
+  
+  
